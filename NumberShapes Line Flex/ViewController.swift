c32be34081ff683b4adbd32590f3c91328168abe
Andrew Fenner <fenner@macintosh-0026bb56cafc.local>
2015-10-14 06:57:13 -0400

    Initial Commit

---
+++ NumberShapes Line Flex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NumberShapes Line Flex
+//
+//  Created by Barbara L Fenner on 10/14/15.
+//  Copyright © 2015 NumberShapes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NumberShapes Line FlexTests/NumberShapes_Line_FlexTests.swift
@@ -0,0 +1,36 @@
+//
+//  NumberShapes_Line_FlexTests.swift
+//  NumberShapes Line FlexTests
+//
+//  Created by Barbara L Fenner on 10/14/15.
+//  Copyright © 2015 NumberShapes. All rights reserved.
+//
+
+import XCTest
+@testable import NumberShapes_Line_Flex
+
+class NumberShapes_Line_FlexTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
